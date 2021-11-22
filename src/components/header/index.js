ca9ae6926ed473f45ce56a6f9d888270c97ad89b
deyaa1997 <dmosa@mykloud.io>
2021-11-22 12:26:40 +0200

    replace the current logo with logo with text only for desktop

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import { css } from "./header.css";
-import img from "../../images/app-logo.png";
+import img from "../../images/myKloud_logo.png";
+import phoneImg from "../../images/app-logo.png";
 const Header = () => {
+  const [width, setWidth] = React.useState(window.innerWidth);
+  const breakpoint = 768;
+  console.log(width);
   return (
     <div className="header">
       <div className="leftSide">
-        <img src={img} alt="Logo image" className="logo"></img>
-        <h1 className="logoText">myKloud</h1>
+        {width > breakpoint ? (
+          <img src={img} alt="Logo image" className="logo1"></img>
+        ) : (
+          <img src={phoneImg} alt="Logo image" className="logo1"></img>
+        )}
       </div>
       <div className="rightSide">
         <p className="rightText">Already have an account?</p>
