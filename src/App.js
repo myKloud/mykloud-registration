fce992c66dab8fe61aaa3a189ed641eba465012d
deyaa1997 <dmosa@mykloud.io>
2021-11-30 13:59:29 +0200

    Finish Login page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ClientInformations from "./components/clientInformations";
 import Recovery from "./components/recovery";
 import Footer from "./components/footer";
 import Verification from "./components/codeVerification";
+import Login from "./components/login";
 import { Route, Switch } from "react-router-dom";
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
       <div className="app m-8">
         <Header />
         <Switch>
-          <Route exact path="/" render={() => <Register />} />
+          <Route exact path="/" render={() => <Login />} />
+          <Route exact path="/register" render={() => <Register />} />
           <Route exact path="/info" render={() => <ClientInformations />} />
           <Route exact path="/recovery" render={() => <Recovery />} />
           <Route exact path="/verification" render={() => <Verification />} />
---
+++ src/components/login/index.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import "./style.scss";
+import Input from "../common/input";
+
+const Login = () => {
+  const [login, setLogin] = useState("email");
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const history = useHistory();
+
+  const emailLogin = () => {
+    return (
+      <>
+        <h1 className="form_title">Login to your account</h1>
+        <p className="normal_text mb-10">Enter your username</p>
+        <div className="mb-5">
+          <div className="user_name">
+            <Input
+              type="text"
+              autofocus={true}
+              value={user}
+              onChange={setUser}
+              placeholder="Username"
+            />
+            <span className="domain">@mykloud.io</span>
+          </div>
+          <p className="note mt-3">Forgot your username?</p>
+        </div>
+        <button className="next_btn" onClick={() => setLogin("password")}>
+          Next
+        </button>
+      </>
+    );
+  };
+
+  const passwordLogin = () => {
+    return (
+      <>
+        <h1 className="form_title">Welcome back!</h1>
+        <p className="normal_text mb-10">{`${user}@mykloud.io`}</p>
+        <div className="mb-5 relative">
+          <Input
+            type={showPassword ? "text" : "password"}
+            value={password}
+            onChange={setPassword}
+            className="extra-padding"
+            placeholder="password"
+          />
+
+          <button
+            className="input_visibilty"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {!showPassword ? (
+              <u style={{ color: "#1565d8" }}>Show</u>
+            ) : (
+              <u style={{ color: "#1565d8" }}>Hide</u>
+            )}
+          </button>
+          <p className="note mt-3">Forgot your password?</p>
+        </div>
+        <button className="next_btn">Login</button>
+      </>
+    );
+  };
+
+  return (
+    <>
+      <div className="form_container login_container">
+        <div className="form_wrapper">
+          {login === "email" ? emailLogin() : passwordLogin()}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
