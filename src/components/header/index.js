b0808cb7b44a16bd842d907827654ce73e41c2f6
deyaa1997 <dmosa@mykloud.io>
2021-11-30 14:08:11 +0200

    fix header in login and register

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -13,8 +13,19 @@ const Header = () => {
           <div className="mykloud_logo" onClick={redirect_url} />
         </div>
         <div className="right_side">
-          <p className="right_text mr-2">Already have an account?</p>
-          <button className="signin_btn">Sign in</button>
+          {window.location.href === "http://localhost:3000/" ? (
+            <>
+              <p className="right_text mr-2">Don’t have an account?</p>
+              <button className="signin_btn">Register</button>{" "}
+            </>
+          ) : (
+            <>
+              <p className="right_text mr-2">Already have an account?</p>
+              <button className="signin_btn">Sign in</button>{" "}
+            </>
+          )}
+          {/* <p className="right_text mr-2">Already have an account?</p>
+          <button className="signin_btn">Sign in</button> */}
         </div>
       </div>
     </>
