5d8d0bf55d650b8b3e6ca09879a770b0c825dfbb
deyaa1997 <dmosa@mykloud.io>
2021-11-30 14:26:37 +0200

    modify header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,6 +6,13 @@ const Header = () => {
     window.location = "http://www.mykloud.io";
   };
 
+  let description = "Don’t have an account?";
+  let action = "Register";
+
+  if (window.location.pathname !== "/") {
+    description = "Already have an account?";
+    action = "Sign in";
+  }
   return (
     <>
       <div className="header_container mb-8">
@@ -13,19 +20,8 @@ const Header = () => {
           <div className="mykloud_logo" onClick={redirect_url} />
         </div>
         <div className="right_side">
-          {window.location.href === "http://localhost:3000/" ? (
-            <>
-              <p className="right_text mr-2">Don’t have an account?</p>
-              <button className="signin_btn">Register</button>{" "}
-            </>
-          ) : (
-            <>
-              <p className="right_text mr-2">Already have an account?</p>
-              <button className="signin_btn">Sign in</button>{" "}
-            </>
-          )}
-          {/* <p className="right_text mr-2">Already have an account?</p>
-          <button className="signin_btn">Sign in</button> */}
+          <p className="right_text mr-2">{description}</p>
+          <button className="signin_btn">{action}</button>{" "}
         </div>
       </div>
     </>
