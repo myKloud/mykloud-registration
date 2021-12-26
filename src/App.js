02d110566bbf260a97791a6bb392cb2b6ae1927e
DeyaaMutleq <dmosa@mykloud.io>
2021-12-26 16:00:23 +0200

    Add Router React Component to make App more clean and readable

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
+import Router from "./Router";
+import pathChecker from "./pathChecker.js";
 import { useHistory, useLocation } from "react-router-dom";
 import { connect } from "react-redux";
 import { React, useEffect } from "react";
-import Header from "./components/header";
-import Register from "./components/register";
-import ClientInformations from "./components/clientInformations";
-import Recovery from "./components/recovery";
-import Footer from "./components/footer";
-import Verification from "./components/codeVerification";
-import Login from "./components/login";
-import Dob from "./components/DOB";
-import ForgetUserName from "./components/forgetUserName/recovery";
-import ForgetPassword from "./components/forgePassword/recovery";
-import { Route, Switch, Redirect } from "react-router-dom";
 import { getStorage, removeStorage } from "./config/storage";
-import Welcome from "./components/welcome";
-import pathChecker from "./pathChecker.js";
 
 function App(props) {
   const HISTORY = useHistory();
@@ -34,24 +23,7 @@ function App(props) {
 
   return (
     <>
-      <Header />
-      <div className="app m-8">
-        <Switch>
-          <Route exact path="/">
-            <Redirect to="/register" />
-          </Route>
-          <Route exact path="/login" render={() => <Login />} />
-          <Route exact path="/register" render={() => <Register />} />
-          <Route exact path="/info" render={() => <ClientInformations />} />
-          <Route exact path="/recovery" render={() => <Recovery />} />
-          <Route exact path="/verification" render={() => <Verification />} />
-          <Route exact path="/dob" render={() => <Dob />} />
-          <Route exact path="/welcome" render={() => <Welcome />} />
-          <Route exact path="/forgetUser" render={() => <ForgetUserName />} />
-          <Route exact path="/forgetPass" render={() => <ForgetPassword />} />
-        </Switch>
-      </div>
-      <Footer />
+      <Router />
     </>
   );
 }
---
+++ src/Router.js
@@ -0,0 +1,39 @@
+import Header from "./components/header";
+import Register from "./components/register";
+import ClientInformations from "./components/clientInformations";
+import Recovery from "./components/recovery";
+import Footer from "./components/footer";
+import Verification from "./components/codeVerification";
+import Login from "./components/login";
+import Dob from "./components/DOB";
+import ForgetUserName from "./components/forgetUserName/recovery";
+import ForgetPassword from "./components/forgePassword/recovery";
+import Welcome from "./components/welcome";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+const Router = () => {
+  return (
+    <>
+      <Header />
+      <div className="app m-8">
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/register" />
+          </Route>
+          <Route exact path="/login" render={() => <Login />} />
+          <Route exact path="/register" render={() => <Register />} />
+          <Route exact path="/info" render={() => <ClientInformations />} />
+          <Route exact path="/recovery" render={() => <Recovery />} />
+          <Route exact path="/verification" render={() => <Verification />} />
+          <Route exact path="/dob" render={() => <Dob />} />
+          <Route exact path="/welcome" render={() => <Welcome />} />
+          <Route exact path="/forgetUser" render={() => <ForgetUserName />} />
+          <Route exact path="/forgetPass" render={() => <ForgetPassword />} />
+        </Switch>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Router;
